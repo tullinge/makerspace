453f45616f22e2394242352ae75bc8ca16aa5ad6
Einar Persson <me@einarpersson.com>
2020-12-09 08:19:08 +0100

    Add wiki folder to gatsby filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `wiki`,
+        path: `${__dirname}/wiki`,
+      },
+    },
     `gatsby-transformer-remark`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
