0f34cb666bdd9d0fd7ea2b06393dda9e3629098e
Einar Persson <me@einarpersson.com>
2020-12-16 14:36:51 +0100

    Updates

--- component---src-pages-index-tsx-8bdf7c6c1d9703bccbd5.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{QeBL:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),l=a.n(n),r=a("Bl7J"),u=a("vrFN");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(u.a,{title:"Home"}),l.a.createElement("h1",null,"Välkommen!"),l.a.createElement("p",null,"Här hittar du massor av hjälp för ditt skapande."))}}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-8bdf7c6c1d9703bccbd5.js.map
